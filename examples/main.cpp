9a56f6144b9ef6410c44f587673f1f64c3bd0963
Oni Dango <willm.kevin@gmail.com>
2026-06-15 00:55:58 +0200

    Updated example to show current Job class basic use case.

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <JobSystem.hpp>
+
+// Exemple d'utilisation du JobSystem
+void myJobFunction(Job& job) {
+    std::cout << "Je travaille sur le job\n";
+    // insérer ici le corps de la fonction du job
+}
 
 int main() {
-	std::cout << "Examples placeholder\n";
-	return 0;
+    // construire un Job en fournissant la fonction, sans parent
+    Job j(&myJobFunction, nullptr);
+
+	// exécuter le job
+    j.run();
+    return 0;
 }
